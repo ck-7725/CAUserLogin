525c626e5c421efc7c669106b4fa4ac210083590
ck-7725 <cissykuang7725@gmail.com>
2024-10-18 01:56:23 -0400

    Completed Task 2.2

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -32,4 +32,10 @@ public interface LoginUserDataAccessInterface {
      * @param name name of user
      */
     void setCurrentUser(String name);
+
+    /**
+     * Gets the current user.
+     * @return name of current user.
+     */
+    String getCurrentUser();
 }
